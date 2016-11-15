cb512cb863814d86d84b30252766c3c603069fac
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2016-11-15 23:04:00 +0600

    Add solution for problem `B - Anton and Digits` Link: https://codeforces.com/contest/734/problem/B Tags: brute force, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/734/submission/22236079

---
+++ codeforces/734/B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+
+using namespace std;
+int main()
+{
+    int a, sum  = 0;
+    int k2 , k3, k5, k6;
+    scanf("%d %d %d %d", &k2 ,&k3, &k5, &k6);
+    while(k2 > 0  && k5 > 0 && k6 > 0 )
+    {
+        k2--;
+        //k3--;
+        k5--;
+        k6--;
+        sum += 256;
+    }
+    while(k2 > 0 && k3 > 0)
+    {
+        k2--;
+        k3--;
+        sum += 32;
+    }
+    printf("%d\n", sum);
+    return 0;
+
+}
