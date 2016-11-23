3f491fd3e46fcf3d68b2a35c0a26a351f8d1f319
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2016-11-23 20:55:00 +0600

    Add solution for problem `B - Xenia and Ringroad` Link: https://codeforces.com/contest/339/problem/B Tags: implementation, *1000 Ref: https://codeforces.com/contest/339/submission/22425833

--- codeforces/339/B.cpp
+++ codeforces/339/B.cpp
@@ -8,25 +8,24 @@ using namespace std;
 int main()
 {
     int n, t,m , i;
-   long long  int temp1, temp2; //ara[100005];
+   long long  int ara[100005];
     scanf("%d %d" ,&n, &m);
-    scanf("%I64d",&temp1);
-    long long int ans = temp1 - 1;
-  if(m > 1)
-    {
-        for(int i = 1; i < m; i++){
-            scanf("%I64d",&temp2);
-            if(temp1 <= temp2)
-                ans += (temp2 - temp1);
-            else
-                ans += (n - temp1) + temp2;
+    for(int i = 1; i <= m; i++)
+        scanf("%I64d",&ara[i]);
+    long long int ans =  ara[1] - 1;
 
-            temp1 = temp2;
+    if( m > 1)
+    {
+        for(int i = 2; i <= m ; i++)
+        {
+            if(ara[i-1] <= ara[i])
+                ans += ara[i] - ara[i-1];
+            else
+            {
+                ans += (n - ara[i-1]) + ara[i];
+            }
         }
     }
-    //long long int ans =  ara[1] - 1;
-
-
     printf("%I64d\n", ans);
 
     return 0;
