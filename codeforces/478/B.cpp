3a5be27fcace0a956c10ce774b8adb697dabf5b7
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2016-11-27 15:25:00 +0600

    Add solution for problem `B - Random Teams` Link: https://codeforces.com/contest/478/problem/B Tags: combinatorics, constructive algorithms, greedy, math, *1300 Ref: https://codeforces.com/contest/478/submission/22523415

---
+++ codeforces/478/B.cpp
@@ -0,0 +1,21 @@
+//#inlcude <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+   // #include<stdio.h>
+//int main()
+
+	long long int a,b;
+	scanf("%I64d %I64d",&a,&b);
+	printf("%I64d %I64d\n",(b*(a/b)*(a/b-1))/2+(a%b)*(a/b),((a-b+1)*(a-b))/2);
+	//return 0;
+
+
+    return 0;
+
+}
