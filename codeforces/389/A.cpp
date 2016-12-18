f235afcb788a2db14b66fde13517e6da3a4a81cf
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2016-12-18 16:16:00 +0600

    Add solution for problem `A - Fox and Number Game` Link: https://codeforces.com/contest/389/problem/A Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/389/submission/23081571

--- codeforces/389/A.cpp
+++ codeforces/389/A.cpp
@@ -1,33 +1,46 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <complex>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cmath>
+#include <map>
+#include <set>
 using namespace std;
-#define pb push_back
-#define all(x) (x).begin(),(x).end()
-#define SZ(x) ((int)(x).size())
-typedef long long ll;
-typedef pair<int,int> PII;
-const int up = 1e9;
-#define loop(i , a , b) for(int i = int(a);  i <= int(b); i++)
-#define vi(x) vector<int> x;
- long long   gcd(long long  a, long long  b)
-{
-   // long long gcd(long long a, long long b)
+//#pragma comment(linker,"/STACK:102400000,102400000")
 
+long long gcd(long long a, long long b)
+{
 	if(a == 0 || b == 0)return a + b;
 	return gcd(b, a % b);
+}
 
+int MAIN()
+{
+	int n;
+	cin >> n;
+	int g = 0;
+	for(int i = 1; i <= n; i++)
+	{
+		int t;
+		cin >> t;
+		g = gcd(g, t);
+	}
+	cout << g * n << endl;
+	return 0;
 }
 
 int main()
 {
-    int n, temp;
-    cin >> n;
-    int g = 0;
-    loop(i, 0, n-1)
-    {
-       cin >> temp;
-       g = gcd(g , temp);
-
-    }
-    cout << n * g << endl;
-   return 0 ;
+	#ifdef LOCAL_TEST
+		freopen("in.txt", "r", stdin);
+		freopen("out.txt", "w", stdout);
+	#endif
+	ios :: sync_with_stdio(false);
+	cout << fixed << setprecision(16);
+	return MAIN();
 }
