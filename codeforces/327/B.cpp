bfa88ece300b5af9e8bfa883c910f2c7e7145b41
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2017-03-11 22:41:00 +0600

    Add solution for problem `B - Hungry Sequence` Link: https://codeforces.com/contest/327/problem/B Tags: math, *1200 Ref: https://codeforces.com/contest/327/submission/25412346

---
+++ codeforces/327/B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define PII pair < int , int >
+#define  PI 2.0*acos(0.0)
+#define ll long long int
+#define loop(i, a, n) for(int i = int (a); i <= int (n); i++)
+#define vi vector < int >
+#define pb push_back
+#define ceil(a, b) (a %b == 0)?(a/b):(a/b)+1
+ll const inf =  1LL << 59 ;
+int  const MX = 1e7 + 8  ;
+int const EPS = 1e-9;
+int  const MOD = 1e9 + 7;
+using namespace std;
+
+int main()
+{
+   int n;
+   cin >> n;
+  // printf("3 ");
+  int h = 1e6;
+   for(int i = 1; i <= n; i++)
+    printf("%d ", h + i);
+
+    return 0;
+}
