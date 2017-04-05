d2a6cc141952fb945e13284ad66f95eefd99cedb
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2017-04-05 21:53:00 +0600

    Add solution for problem `C - Palindrome Transformation` Link: https://codeforces.com/contest/486/problem/C Tags: brute force, greedy, implementation, *1700 Ref: https://codeforces.com/contest/486/submission/26147684

---
+++ codeforces/486/C.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 100100;
+
+int n, p, res = 0;
+bool ok[N];
+string s;
+
+int main() {
+    cin >> n >> p;
+    cin >> s;
+    s = "Q" + s;
+    for(int i = 1; i <= n / 2; i++) {
+        if(s[i] != s[n + 1 - i]) {
+            if(abs(p - i) > abs(p - (n + 1 - i))) ok[n + 1 - i] = 1;
+            else ok[i] = 1;
+            int them = abs(s[i] - s[n + 1 - i]);
+            them = min(them, 26 - them);
+            res += them;
+        }
+    }
+    int vt1 = p, vt2 = p;
+    for(int i = 1; i < p; i++)
+    if(ok[i]) {
+        vt1 = i;
+        break;
+    }
+    for(int i = n; i > p; i--)
+    if(ok[i]) {
+        vt2 = i;
+        break;
+    }
+    res += abs(p - vt1) + abs(p - vt2);
+    res += min(abs(p -vt1), abs(p - vt2));
+    cout << res;
+    return 0;
+}
