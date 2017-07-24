63a158fe99c9a575a23abdcfffb2329e34cdcb40
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2017-07-24 20:38:00 +0600

    Add solution for problem `A - Sasha and Sticks` Link: https://codeforces.com/contest/832/problem/A Tags: games, math, *800 Ref: https://codeforces.com/contest/832/submission/28828077

---
+++ codeforces/832/A.cpp
@@ -0,0 +1,63 @@
+//HK HK HK HK HK 108 times
+
+
+#include <bits/stdc++.h>
+#define PII pair < int , int >
+#define PI 2.0*acos(0.0)
+#define ll long long int
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define FOR(i, a, n) for(int i = int (a); i < int (n); i++)
+#define vi vector < int >
+#define pb push_back
+#define ceiling(a, b) (a %b == 0)?(a/b):(a/b)+1
+#define F first
+#define S second
+
+int const up =   1e6 + 10 ;
+int const up1 = 5 * 1e5 + 10  ;
+int const eps = 1e-9;
+int const MX =   5 * 1e5 + 10;
+int const mod = 1000000007 ;
+using namespace std;
+ long long bigmod(long long p,long long e,long long M){
+    long long ret = 1;
+    for(; e > 0; e >>= 1){
+        if(e & 1) ret = (ret * p) % M;
+        p = (p * p) % M;
+    } return ret;
+}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+ ll modinverse(ll a,ll M){return bigmod(a,M-2,M);}
+
+// ************************************************************** GLOBAL VARIABLES *******************************************
+
+
+
+ //******************************************************************************************************************************
+
+
+ll bigmod(ll base, ll pow)
+{
+    if(pow == 0) return (1 % mod);
+    ll x = bigmod(base, pow/2 );
+    x = ( x * x ) % mod;
+    if(pow % 2 == 1)
+         x = (x * base) % mod;
+    return x;
+}
+
+int main()
+{
+  ll n, k;
+  cin >> n >> k;
+  ll tmp = (n/k);
+  if(tmp % 2 == 1){
+    cout << "yes"<< endl;
+  }
+  else
+    cout << "no" << endl;
+
+   return 0;
+}
