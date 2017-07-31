10d6bb887a9a7a8d9b0b73ec1ba4345bd4e7f349
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2017-07-31 20:41:00 +0600

    Add solution for problem `A - Key races` Link: https://codeforces.com/contest/835/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/835/submission/29053456

---
+++ codeforces/835/A.cpp
@@ -0,0 +1,69 @@
+//HK HK HK HK HK 108 times
+
+
+#include <bits/stdc++.h>
+#define PII pair < int , int >
+#define PI 2.0*acos(0.0)
+#define ll long long int
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define FOR(i, a, n) for(int i = int (a); i < int (n); i++)
+#define vi vector < int >
+#define pb push_back
+#define ceiling(a, b) (a %b == 0)?(a/b):(a/b)+1
+#define F first
+#define S second
+
+int const up =   1e6 + 10 ;
+int const up1 = 5 * 1e5 + 10  ;
+int const eps = 1e-9;
+int const MX =   5 * 1e5 + 10;
+int const mod = 1000000007 ;
+using namespace std;
+ long long bigmod(long long p,long long e,long long M){
+    long long ret = 1;
+    for(; e > 0; e >>= 1){
+        if(e & 1) ret = (ret * p) % M;
+        p = (p * p) % M;
+    } return ret;
+}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+ ll modinverse(ll a,ll M){return bigmod(a,M-2,M);}
+
+// ************************************************************** GLOBAL VARIABLES *******************************************
+
+
+
+ //******************************************************************************************************************************
+ll bigmod(ll base, ll pow)
+{
+    if(pow == 0) return (1 % mod);
+    ll x = bigmod(base, pow/2 );
+    x = ( x * x ) % mod;
+    if(pow % 2 == 1)
+         x = (x * base) % mod;
+    return x;
+}
+
+
+int main()
+{
+  int s, v1 ,v2 , t1 ,t2;
+  int tmp1 , tmp2 ;
+  cin >> s >> v1 >> v2 >> t1 >> t2;
+  tmp1 = s * v1 + 2 * t1;
+  tmp2 = s * v2 + 2 * t2;
+  if(tmp1 == tmp2){
+    puts("Friendship");
+
+  }
+  else if(tmp1 < tmp2) {
+     puts("First");
+  }
+  else {
+    puts("Second");
+  }
+  return 0;
+}
+
