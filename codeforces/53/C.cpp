0fab3dfa92379b48790877b685d6f63316a73805
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2017-11-11 15:35:00 +0600

    Add solution for problem `C - Little Frog` Link: https://codeforces.com/contest/53/problem/C Tags: constructive algorithms, *1200 Ref: https://codeforces.com/contest/53/submission/32215617

---
+++ codeforces/53/C.cpp
@@ -0,0 +1,60 @@
+//HK HK HK HK HK 108 times
+#include <bits/stdc++.h>
+#define PII pair < int , int >
+#define PI 2.0*acos(0.0)
+#define ll long long int
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define FOR(i, a, n) for(int i = int (a); i < int (n); i++)
+#define vi vector < int >
+#define pb push_back
+#define ceiling(a, b) (a %b == 0)?(a/b):(a/b)+1
+#define F first
+#define S second
+int const MX = 2e6+ 10 ;
+int const mod = 1000000007 ;
+
+using namespace std;
+
+ long long bigmod(long long p,long long e,long long M){
+    long long ret = 1;
+    for(; e > 0; e >>= 1){
+        if(e & 1) ret = (ret * p) % M;
+        p = (p * p) % M;
+    } return ret;
+}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+ ll modinverse(ll a,ll M){return bigmod(a,M-2,M);}
+
+// ************************************************************** GLOBAL VARIABLES *******************************************
+
+
+
+ //******************************************************************************************************************************
+ll bigmod(ll base, ll pow){
+   if(pow == 0) return (1 % mod);
+    ll x = bigmod(base, pow/2 );
+    x = ( x * x ) % mod;
+    if(pow % 2 == 1)
+         x = (x * base) % mod;
+    return x;
+}
+
+int main()
+{
+    ll a;
+
+    while(cin >> a)
+    {
+        for(int i = (a/2)+(a%2);i > 0 ;i--)
+        {
+            if(i != (a+1 - i)){
+                printf("%d %d ",i, a + 1-i);
+            }else{
+                printf("%d ",i);
+            }
+        }
+    }
+    return 0 ;
+}
