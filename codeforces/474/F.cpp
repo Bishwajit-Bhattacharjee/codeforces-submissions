4a3a4498b4ea8ecb290ddd2444b743c25630b91f
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2018-03-20 22:29:00 +0600

    Add solution for problem `F - Ant colony` Link: https://codeforces.com/contest/474/problem/F Tags: data structures, math, number theory, *2100 Ref: https://codeforces.com/contest/474/submission/36437960

---
+++ codeforces/474/F.cpp
@@ -0,0 +1,174 @@
+/// HK 108 Times
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using namespace std ;
+#define ll long long
+#define pb push_back
+typedef pair <int,int>  PII ;
+vector < PII > given ;
+vector < pair < int , PII >  >  edge  ;
+typedef pair < ll, ll > PLL ;
+#define all(x) (x).begin() , (x).end()
+#define F first
+#define S second
+#define READ freopen("in.txt","r",stdin)
+#define WRITE freopen("out.txt","w",stdout)
+#define FAST ios_base::sync_with_stdio(false)
+#define ll long long
+#define MOD 1000000007
+#define endl '\n'
+#define DEBUG(x) cout << #x << ' ' << x << endl
+
+// Order Statistic Tree
+
+/* Special functions:
+
+        find_by_order(k) --> returns iterator to the kth largest element counting from 0
+        order_of_key(val) --> returns the number of items in a set that are strictly smaller than our item
+*/
+const int mod = 1e9 + 7 ;
+typedef tree<
+int,					// type long long
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+long long bigmod(long long p,long long e,long long M){
+    long long ret = 1;
+    for(; e > 0; e >>= 1){
+        if(e & 1) ret = (ret * p) % M;
+        p = (p * p) % M;
+    } return ret;
+}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+ ll modinverse(ll a,ll M){return bigmod(a,M-2,M);}
+ll bigmod(ll base, ll pow)
+{
+    if(pow == 0) return (1 % mod);
+    ll x = bigmod(base, pow/2 );
+    x = ( x * x ) % mod;
+    if(pow % 2 == 1)
+         x = (x * base) % mod;
+    return x;
+}
+const int MX = 1e5 + 10 ;
+int ara[MX] ;
+
+struct node
+{
+    int num;
+    int cnt ;
+
+    node()
+    {
+        num = cnt = 0;
+
+    }
+    node(int n, int cn):num(n),cnt(cn)
+    {
+
+    }
+    bool operator==(const node& rhs)const
+    {
+         return num == rhs.num && cnt == rhs.cnt;
+    }
+    void print()
+    {
+        cout << num << ' ' << cnt << endl;
+    }
+};
+
+node tree1[MX*4] ;
+
+
+node combine(node &a , node &b)
+{
+    if(a.num == -1 ) return b;
+    if(b.num == -1 ) return a;
+
+    if(a.num == b.num)
+    {
+        return node(a.num, a.cnt + b.cnt);
+    }
+    else if(a.num % b.num == 0 )
+    {
+        return node(b.num, b.cnt);
+    }
+    else if(b.num % a.num == 0)
+        return a;
+
+    else {
+        return node(__gcd(a.num, b.num), 0);
+    }
+    //return tmp;
+}
+
+void build(int now, int ss, int se)
+{
+    if(ss == se)
+    {
+        tree1[now] = node(ara[ss],1);
+        return ;
+    }
+
+    int mid = (ss + se) >> 1;
+    build(now*2, ss, mid ) ;
+    build(now * 2 + 1, mid + 1 , se);
+
+    tree1[now] = combine(tree1[now*2] , tree1[now*2 + 1]);
+
+//    cout << "range " << ss << ' ' << se << endl;
+//    cout << "value " ;
+//    tree1[now].print();
+
+
+}
+
+node query(int now, int ss, int se, int qs, int qe)
+{
+    if(ss > qe or se < qs) return node(-1,0);
+
+    if(qs <= ss and se <= qe) return tree1[now];
+
+    int mid = (ss + se) >> 1;
+
+    node left = query(now*2, ss, mid ,qs,qe) ;
+    node rgt = query(now * 2 + 1, mid + 1, se, qs, qe);
+
+    return combine(left, rgt);
+
+}
+
+int main()
+{
+    //clock_t z = clock();
+    // READ;
+
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+        cin >> ara[i] ;
+
+    int m;
+    cin >> m;
+    build(1,1,n);
+
+    while(m--)
+    {
+        int l, r;
+        cin >> l >> r;
+        cout << (r-l+1) - query(1,1,n,l,r).cnt << endl;
+    }
+    //printf("Total Time: %.3f\n", (double)(clock() - z) / CLOCKS_PER_SEC);
+
+    return 0;
+}
