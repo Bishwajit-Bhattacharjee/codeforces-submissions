efc123b7404d1b0d06903f3745e1e27e80dd0cf1
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2018-04-29 12:04:00 +0600

    Add solution for problem `D - Magic Box` Link: https://codeforces.com/contest/231/problem/D Tags: brute force, geometry, *1600 Ref: https://codeforces.com/contest/231/submission/37701268

---
+++ codeforces/231/D.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        int x,y,z;
+        cin >> x >> y >> z;
+        int ans = 0;
+        int a1,a2,a3,a4,a5,a6;
+
+        int x1,y1, z1;
+
+        cin >> x1 >> y1 >> z1;
+        cin >> a1 >> a2 >> a3 >> a4 >> a5 >> a6;
+
+        if(y < 0 )
+                ans += a1;
+        if(y > y1)
+                ans += a2;
+        if(z < 0 )
+                ans += a3;
+        if(z > z1)
+                ans += a4;
+        if(x < 0)
+                ans += a5;
+        if(x > x1 )
+                ans += a6;
+        cout << ans << endl;
+        return 0;
+}
