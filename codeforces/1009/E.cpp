eb6b54e0d241978817f19cd3db3f5268a02405c9
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2018-07-16 23:25:00 +0600

    Add solution for problem `E - Intercity Travelling` Link: https://codeforces.com/contest/1009/problem/E Tags: combinatorics, math, probabilities, *2000 Ref: https://codeforces.com/contest/1009/submission/40448537

---
+++ codeforces/1009/E.cpp
@@ -0,0 +1,93 @@
+/// HK 108 Times
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using namespace std ;
+#define ll long long
+#define pb push_back
+typedef pair <int,int>  PII ;
+vector < PII > given ;
+vector < pair < int , PII >  >  edge  ;
+typedef pair < ll, ll > PLL ;
+#define all(x) (x).begin() , (x).end()
+#define F first
+#define S second
+#define READ freopen("in.txt","r",stdin)
+#define WRITE freopen("out.txt","w",stdout)
+#define FAST ios_base::sync_with_stdio(false)
+#define ll long long
+#define MOD 1000000007
+#define endl '\n'
+
+// Order Statistic Tree
+
+/* Special functions:
+
+        find_by_order(k) --> returns iterator to the kth largest element counting from 0
+        order_of_key(val) --> returns the number of items in a set that are strictly smaller than our item
+*/
+const int mod = 1e9 + 7 ;
+typedef tree<
+int,					// type long long
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+long long bigmod(long long p,long long e,long long M){
+    if(e < 0 )
+        return 0;
+    long long ret = 1;
+    for(; e > 0; e >>= 1){
+        if(e & 1) ret = (ret * p) % M;
+        p = (p * p) % M;
+    } return ret;
+}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+ ll modinverse(ll a,ll M){return bigmod(a,M-2,M);}
+ll bigmod(ll base, ll pow)
+{
+    if(pow == 0) return (1 % mod);
+    ll x = bigmod(base, pow/2 );
+    x = ( x * x ) % mod;
+    if(pow % 2 == 1)
+         x = (x * base) % mod;
+    return x;
+}
+const int MX = 1e6 + 10 ;
+ll tmpMOD = 998244353;
+
+int main()
+{
+    FAST;
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector < ll > ara(n+1);
+    ll ans = 0LL;
+    for(int i = 1; i <= n;i++)
+        cin >> ara[i];
+    vector < ll > big(n+1) ;
+    big[0] = 1;
+    for(int i = 1; i <= n; i++)
+        big[i] = (big[i-1] * 2 ) % tmpMOD;
+
+    ll term = ans = (ara[1] * big[n-1]) % tmpMOD;
+
+    for(int i = 2; i <= n ;i++)
+    {
+       // scanf("%I64d",&ara[i]) ;
+        term = (term - (ara[i-1] * (big[n-i])) + tmpMOD + ara[i]*big[n-i])  % tmpMOD ;
+        //ans %= tmpMOD ;
+        ans = (ans + term) ;
+        if(ans >= tmpMOD)
+            ans -= tmpMOD ;
+
+    }
+    cout << ans << endl;
+    return 0;
+}
