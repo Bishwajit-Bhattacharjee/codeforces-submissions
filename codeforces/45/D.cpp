c95ca6ef25e64d6edf4b3342ba0163ec79841592
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2018-07-18 00:54:00 +0600

    Add solution for problem `D - Event Dates` Link: https://codeforces.com/contest/45/problem/D Tags: greedy, meet-in-the-middle, sortings, *1900 Ref: https://codeforces.com/contest/45/submission/40489085

---
+++ codeforces/45/D.cpp
@@ -0,0 +1,108 @@
+/// HK 108 Times
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using namespace std ;
+#define ll long long
+#define pb push_back
+typedef pair <int,int>  PII ;
+vector < PII > given ;
+vector < pair < int , PII >  >  edge  ;
+typedef pair < ll, ll > PLL ;
+#define all(x) (x).begin() , (x).end()
+#define F first
+#define S second
+#define READ freopen("in.txt","r",stdin)
+#define WRITE freopen("out.txt","w",stdout)
+#define FAST ios_base::sync_with_stdio(false)
+#define ll long long
+#define MOD 1000000007
+#define endl '\n'
+
+// Order Statistic Tree
+
+/* Special functions:
+
+        find_by_order(k) --> returns iterator to the kth largest element counting from 0
+        order_of_key(val) --> returns the number of items in a set that are strictly smaller than our item
+*/
+const int mod = 1e9 + 7 ;
+typedef tree<
+int,					// type long long
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+long long bigmod(long long p,long long e,long long M){
+    if(e < 0 )
+        return 0;
+    long long ret = 1;
+    for(; e > 0; e >>= 1){
+        if(e & 1) ret = (ret * p) % M;
+        p = (p * p) % M;
+    } return ret;
+}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+ ll modinverse(ll a,ll M){return bigmod(a,M-2,M);}
+ll bigmod(ll base, ll pow)
+{
+    if(pow == 0) return (1 % mod);
+    ll x = bigmod(base, pow/2 );
+    x = ( x * x ) % mod;
+    if(pow % 2 == 1)
+         x = (x * base) % mod;
+    return x;
+}
+const int MX = 1e6 + 10 ;
+
+int random(int l,int r)
+{
+    int range = r - l + 1;
+    return l + (rand() % range ) ;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector < pair < PII, int>  > ara(n);
+
+    for(int i = 0; i < n; i++)
+        cin >> ara[i].F.F >> ara[i].F.S, ara[i].S = i ;
+
+    sort(all(ara)) ;
+
+    int id = 0;
+    priority_queue < PII > pq;
+    vector < int > ans(n) ;
+    int cur = ara[0].F.F;
+    while(id < n )
+    {
+        cur = ara[id].F.F;
+        while( (id < n and ara[id].F.F <= cur) or !pq.empty())
+        {
+            while(id < n and ara[id].F.F <= cur )
+            {
+                pq.push({-ara[id].F.S, -ara[id].S});
+                id++;
+            }
+
+            if(!pq.empty())
+            {
+                PII top = pq.top();
+                top.F *= -1;
+                top.S *= -1;
+                pq.pop();
+                ans[top.S] = cur++;
+            }
+        }
+    }
+
+    for(int i = 0 ; i < n; i++)
+        cout << ans[i] << " " ;
+    return 0;
+}
