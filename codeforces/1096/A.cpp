a61c275a3d750c354f8277f23101d421cb2fa1b5
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2018-12-30 12:55:00 +0600

    Add solution for problem `A - Find Divisible` Link: https://codeforces.com/contest/1096/problem/A Tags: greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1096/submission/47714914

---
+++ codeforces/1096/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 100000 + 5;
+vector<int> g[N];
+typedef pair < int , int > PII;
+typedef long long int ll;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int l,r;
+        cin >> l >> r;
+        cout << l << " " << 2*l << '\n';
+    }
+    return 0;
+}
+
+
+
