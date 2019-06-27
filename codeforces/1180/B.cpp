5b2a1074cc81dda58da6a38525aa9994aa9ec515
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2019-06-27 23:00:00 +0600

    Add solution for problem `B - Nick and Array` Link: https://codeforces.com/contest/1180/problem/B Tags: greedy, implementation, *1500 Ref: https://codeforces.com/contest/1180/submission/56171802

---
+++ codeforces/1180/B.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef pair<int,int> PII;
+typedef long long int ll;
+typedef pair< ll,ll> PLL;
+typedef unsigned long long int ULL;
+typedef pair<ULL,int> PUI;
+typedef tree<
+PLL,
+null_type,
+less<PLL>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+
+PLL operator+(PLL a,PLL b){ return {a.first + b.first, a.second + b.second}; }
+PLL operator-(PLL a,PLL b){ return {a.first - b.first, a.second - b.second}; }
+PLL operator*(PLL a,PLL b){ return {a.first * b.first, a.second * b.second}; }
+PLL operator/(PLL a,PLL b){ return {a.first / b.first, a.second / b.second}; }
+PLL operator%(PLL a,PLL b){ return {( b.first + a.first) % b.first, (b.second + a.second) % b.second}; }
+PLL operator%(PLL a,ll b){ return {a.first % b, a.second % b} ;}
+PLL operator+(PLL a,ll b){ return {a.first + b, a.second + b} ;}
+PLL operator-(PLL a,ll b){ return {a.first - b, a.second - b} ;}
+PLL operator*(PLL a,ll b){ return {a.first * b, a.second * b} ;}
+
+ostream& operator<<(ostream &out,PLL a)
+{
+    out << "( " << a.first << ", " << a.second << " )" ;
+    return out;
+}
+
+ostream& operator<<(ostream &out,PUI a)
+{
+    out << "( " << a.first << ", " << a.second << " )" ;
+    return out;
+}
+/*
+	Order Statistics Tree ( OST )
+
+	find_by_order()
+		takes an integer k and returns an iterator to the k'th largest element counting from zero
+	order_of_key()
+		takes an element, here an integer and returns the number of elements strictly smaller than input
+*/
+
+
+int const MOD = 1e9 + 7;
+int const N = 2e5 + 10;
+
+int opr(int v)
+{
+    return -v - 1;
+}
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);    
+    int n;
+    cin >> n;
+    vector < int > v(n);
+    for(int i = 0; i < n;i++){
+        cin >> v[i] ;
+        if(v[i] >= 0) v[i] = opr(v[i]);
+    }
+    if(n & 1){
+        int val = *min_element(v.begin(),v.end());
+        for(int i = 0; i < n; i++){
+            if(val == v[i]) {
+                v[i] = opr(v[i]);
+                break;
+            }
+        }
+    }
+    for(auto x : v) 
+        cout << x << " ";
+    
+
+    return 0;
+}
