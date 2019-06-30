fc002db64fb5ba8d1c9424b8ceee9ac6cdbf9b60
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2019-06-30 22:17:00 +0600

    Add solution for problem `A - Stickers and Toys` Link: https://codeforces.com/contest/1187/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/1187/submission/56337399

---
+++ codeforces/1187/A.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef pair<int,int> PII;
+typedef long long int ll;
+typedef pair< ll,ll> PLL;
+typedef unsigned long long int ULL;
+typedef pair<ULL,int> PUI;
+#define S second
+#define F first
+typedef tree<
+PLL,
+null_type,
+less<PLL>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+
+PLL operator+(PLL a,PLL b){ return {a.first + b.first, a.second + b.second}; }
+PLL operator-(PLL a,PLL b){ return {a.first - b.first, a.second - b.second}; }
+PLL operator*(PLL a,PLL b){ return {a.first * b.first, a.second * b.second}; }
+PLL operator/(PLL a,PLL b){ return {a.first / b.first, a.second / b.second}; }
+PLL operator%(PLL a,PLL b){ return {( b.first + a.first) % b.first, (b.second + a.second) % b.second}; }
+PLL operator%(PLL a,ll b){ return {a.first % b, a.second % b} ;}
+PLL operator+(PLL a,ll b){ return {a.first + b, a.second + b} ;}
+PLL operator-(PLL a,ll b){ return {a.first - b, a.second - b} ;}
+PLL operator*(PLL a,ll b){ return {a.first * b, a.second * b} ;}
+
+ostream& operator<<(ostream &out,PLL a)
+{
+    out << "( " << a.first << ", " << a.second << " )" ;
+    return out;
+}
+
+ostream& operator<<(ostream &out,PUI a)
+{
+    out << "( " << a.first << ", " << a.second << " )" ;
+    return out;
+}
+/*
+	Order Statistics Tree ( OST )
+
+	find_by_order()
+		takes an integer k and returns an iterator to the k'th largest element counting from zero
+	order_of_key()
+		takes an element, here an integer and returns the number of elements strictly smaller than input
+*/
+
+
+int const MOD = 1e9 + 7;
+int const N = 2e5 + 10;
+
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    ll T,n,s,t;
+    cin >> T;
+    while(T--){
+        cin >> n >> s >> t;
+        ll x =  s + t - n;
+        s -= x;
+        t -= x;
+        cout << max(s,t) + 1 << '\n';
+    }
+    return 0;
+}
