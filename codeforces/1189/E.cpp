b1e67acc27d6f5e0808e8a601cbc69950343a715
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2019-07-07 17:55:00 +0600

    Add solution for problem `E - Count Pairs` Link: https://codeforces.com/contest/1189/problem/E Tags: number theory, *2300 Ref: https://codeforces.com/contest/1189/submission/56668309

---
+++ codeforces/1189/E.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef pair<int,int> PII;
+typedef long long int ll;
+typedef pair< ll,ll> PLL;
+typedef unsigned long long int ULL;
+typedef pair<ULL,int> PUI;
+#define S second
+#define F first
+typedef tree<
+PLL,
+null_type,
+less<PLL>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+
+PLL operator+(PLL a,PLL b){ return {a.first + b.first, a.second + b.second}; }
+PLL operator-(PLL a,PLL b){ return {a.first - b.first, a.second - b.second}; }
+PLL operator*(PLL a,PLL b){ return {a.first * b.first, a.second * b.second}; }
+PLL operator/(PLL a,PLL b){ return {a.first / b.first, a.second / b.second}; }
+PLL operator%(PLL a,PLL b){ return {( b.first + a.first) % b.first, (b.second + a.second) % b.second}; }
+PLL operator%(PLL a,ll b){ return {a.first % b, a.second % b} ;}
+PLL operator+(PLL a,ll b){ return {a.first + b, a.second + b} ;}
+PLL operator-(PLL a,ll b){ return {a.first - b, a.second - b} ;}
+PLL operator*(PLL a,ll b){ return {a.first * b, a.second * b} ;}
+
+ostream& operator<<(ostream &out,PLL a)
+{
+    out << "( " << a.first << ", " << a.second << " )" ;
+    return out;
+}
+
+ostream& operator<<(ostream &out,PUI a)
+{
+    out << "( " << a.first << ", " << a.second << " )" ;
+    return out;
+}
+/*
+	Order Statistics Tree ( OST )
+
+	find_by_order()
+		takes an integer k and returns an iterator to the k'th largest element counting from zero
+	order_of_key()
+		takes an element, here an integer and returns the number of elements strictly smaller than input
+*/
+
+
+int const MOD = 1e9 + 7;
+int const N = 2e5 + 10;
+int const LOG = 20;
+ll n,p,k;
+
+ll calc(ll x)
+{
+    ll ret = 1;
+    ret = (x * x % p * x) % p;
+    ret = (ret - k + p ) % p;
+    ret = (x * ret ) % p;
+    return ret;
+}
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    
+    cin >> n >> p >> k;
+
+    vector < ll > v(n);
+    map < ll, ll > mp;
+    for(int i = 0; i < n; i++){
+        cin >> v[i] ;
+        mp[calc(v[i])]++;
+    }
+    ll ans = 0;
+
+    for(auto x : mp){
+        ans += (x.S * (x.S-1)) / 2;
+    }
+    cout << ans << '\n';
+
+    return 0;
+}
