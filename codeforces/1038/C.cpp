e9263e868b30bde60f6bdbcb6d77fe11b15929de
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2019-12-06 21:01:00 +0600

    Add solution for problem `C - Gambling` Link: https://codeforces.com/contest/1038/problem/C Tags: greedy, sortings, *1300 Ref: https://codeforces.com/contest/1038/submission/66410094

---
+++ codeforces/1038/C.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace std;
+using namespace __gnu_pbds;
+
+double INF = 1e100;
+double EPS = 1e-12;
+typedef long long int ll;
+
+typedef pair < int,int > PII;
+typedef pair < ll,ll > PLL;
+
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+#define F first
+#define S second
+
+
+ostream& operator<<(ostream & os, PLL h)
+{
+	return os << "( " << h.F << ", " << h.S << " )" << endl;
+
+}
+PLL operator+ (PLL a, ll x)     {return {a.F + x, a.S + x} ;}
+PLL operator- (PLL a, ll x)     {return {a.F - x, a.S - x} ;}
+PLL operator* (PLL a, ll x)     {return {a.F * x, a.S * x} ;}
+PLL operator+(PLL x, PLL y) { return {x.F + y.F,x.S + y.S} ;}
+PLL operator-(PLL x,PLL y) { return {x.F - y.F, x.S - y.S} ;}
+PLL operator*(PLL x,PLL y) { return {x.F * y.F , x.S * y.S} ;}
+PLL operator%(PLL x,PLL y) { return {x.F % y.F, x.S % y.S} ;}
+
+//PLL base = {37,41};
+PLL M = {1e9+21, 1e9+9};
+
+int const N = 2e5 + 5;
+int const LOG = 31;
+ll const MOD = 998244353;
+
+ll bigmod(ll a, ll b)
+{
+	if(!b) return 1;
+	ll x  = bigmod(a,b/2);
+
+	x = (x * x)%MOD;
+	if(b&1)
+		x = (x * a) %MOD;
+	return x;
+}
+
+ll inv(ll x)
+{
+	return bigmod(x%MOD,MOD-2);
+}
+int main(){
+
+	ios::sync_with_stdio(false); cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	multiset < int > ma, mb;
+	int tmp;
+	for(int i = 1; i <= n; i++) {
+		cin >> tmp;
+		ma.insert(-tmp);
+	}
+	for(int i = 1; i <= n; i++) {
+		cin >> tmp;
+		mb.insert(-tmp);
+	}
+
+	ll ans = 0;
+
+	for(int i = 1; i <=2*n; i++){
+		if(i & 1){
+			ll can1 = 0, can2 = 0;
+			if(!ma.empty()){
+				can1 = -1 * *(ma.begin());
+			}
+			if(!mb.empty()){
+				can2 = -1 * *(mb.begin());
+			}
+			if(can1 > can2) ans += can1, ma.erase(ma.begin());
+			else 
+				if(!mb.empty())mb.erase(mb.begin());
+		}
+		else {
+			ll can1 = 0, can2 = 0;
+			if(!ma.empty()){
+				can1 = -1 * *(ma.begin());
+			}
+			if(!mb.empty()){
+				can2 = -1 * *(mb.begin());
+			}
+			if(can2 > can1) ans -= can2, mb.erase(mb.begin());
+			else {
+				if(!ma.empty()){
+					ma.erase(ma.begin());
+				}
+			}
+		}
+	}
+	cout << ans << endl;
+/*
+	if((n+1)/2 % 2 == 0){
+		cout << "Yes\n";
+		cout << (1+n)/2 << " ";
+
+		for(int i = 1; i <= n; i+= 2)
+			cout << i << " ";
+		cout << endl;
+
+		cout << n / 2 << " " ;
+		for(int i = 2; i <= n; i += 2)
+			cout << i << " " ;
+		cout << endl;
+
+	}
+	else {
+		if(n == 1) {
+			cout << "No\n";
+		}
+		else {
+			cout << "Yes\n";
+			cout << "3 1 2 3\n";
+			cout << n - 3 << " ";
+
+			for(int i = 4; i <= n; i++) 
+				cout << i << " ";
+			cout << endl;
+		}
+		
+	}
+*/
+	return 0;
+}
