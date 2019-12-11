3b80c1989f6ac6dd1d5055cc4b42821582d6bed1
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2019-12-11 17:40:00 +0600

    Add solution for problem `D - Binary String Minimizing` Link: https://codeforces.com/contest/1256/problem/D Tags: greedy, *1500 Ref: https://codeforces.com/contest/1256/submission/66632387

---
+++ codeforces/1256/D.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace std;
+using namespace __gnu_pbds;
+
+double INF = 1e100;
+double EPS = 1e-12;
+typedef long long int ll;
+
+typedef pair < int,int > PII;
+typedef pair < ll,ll > PLL;
+
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+#define F first
+#define S second
+
+
+ostream& operator<<(ostream & os, PLL h)
+{
+	return os << "( " << h.F << ", " << h.S << " )" << endl;
+
+}
+PLL operator+ (PLL a, ll x)     {return {a.F + x, a.S + x} ;}
+PLL operator- (PLL a, ll x)     {return {a.F - x, a.S - x} ;}
+PLL operator* (PLL a, ll x)     {return {a.F * x, a.S * x} ;}
+PLL operator+(PLL x, PLL y) { return {x.F + y.F,x.S + y.S} ;}
+PLL operator-(PLL x,PLL y) { return {x.F - y.F, x.S - y.S} ;}
+PLL operator*(PLL x,PLL y) { return {x.F * y.F , x.S * y.S} ;}
+PLL operator%(PLL x,PLL y) { return {x.F % y.F, x.S % y.S} ;}
+
+//PLL base = {37,41};
+PLL M = {1e9+21, 1e9+9};
+
+int const N = 2e5 + 5;
+int const LOG = 31;
+ll const MOD = 998244353;
+
+ll bigmod(ll a, ll b)
+{
+	if(!b) return 1;
+	ll x  = bigmod(a,b/2);
+
+	x = (x * x)%MOD;
+	if(b&1)
+		x = (x * a) %MOD;
+	return x;
+}
+
+ll inv(ll x)
+{
+	return bigmod(x%MOD,MOD-2);
+}
+vector < ll > primes;
+vector < bool > marks(N+1,0);
+
+void sieve()
+{
+	marks[1] = 1;
+	for(int i = 2; i < N; i++){
+		if(!marks[i]){
+			for(int j = 2*i; j < N; j += i){
+				marks[j] = 1;
+			}
+			primes.push_back(i);
+		}
+	}
+}
+
+int main(){
+
+	ios::sync_with_stdio(false); cin.tie(0);
+
+	
+	int t;
+	cin >> t;
+
+	while(t--){
+		ll n,k;
+		cin >> n >> k;
+
+		string s;
+		cin >> s;
+
+		deque < int > dq;
+
+		for(int i = 0; i < s.size(); i++){
+			if(s[i]=='1') {
+				dq.push_back(i);
+				continue;
+			}
+			else {
+				if(dq.empty()) continue;
+
+				if(k >= dq.size()){
+					s[dq.front()] = '0';
+					s[i] = '1';
+					k -= dq.size();
+					dq.pop_front();
+					dq.push_back(i);
+				
+				}
+				else {
+					while(k>1){
+						dq.pop_back();
+						k--;
+					}
+					if(k>0)
+						swap(s[i],s[dq.back()]);
+					break;
+				}
+			}
+		}
+		cout << s << '\n';
+
+	}
+	return 0;
+}
