8e2a6076e4e9d6648e7a75e511f7858a4d7a6406
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2020-01-19 15:35:00 +0600

    Add solution for problem `C - Fadi and LCM` Link: https://codeforces.com/contest/1285/problem/C Tags: brute force, math, number theory, *1400 Ref: https://codeforces.com/contest/1285/submission/69089389

---
+++ codeforces/1285/C.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifndef ONLINE_JUDGE
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+typedef long long int ll;
+int main()
+{
+    
+    //    debug() << imie(ans) imie(m);
+    
+	ll n;
+	cin >> n;
+	ll can = 1e12;
+	for(ll i = 1; i * i <= n; i++){
+		if(n % i == 0){
+			ll ii = n / i;
+			if(__gcd(i,ii) == 1){
+				can = min(can, max(i,ii));
+			}
+		}
+	}
+	cout << n / can << " " << can << endl;
+
+    return 0;   
+}
