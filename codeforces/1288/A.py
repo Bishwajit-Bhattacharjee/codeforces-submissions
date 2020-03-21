0c1a380ea54b41677943d015d8c3790c53118e53
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2020-03-21 17:16:00 +0600

    Add solution for problem `A - Deadline` Link: https://codeforces.com/contest/1288/problem/A Tags: binary search, brute force, math, ternary search, *1100 Ref: https://codeforces.com/contest/1288/submission/73880461

--- codeforces/1288/A.py
+++ codeforces/1288/A.py
@@ -1,19 +1,13 @@
 import math 
-
-def solve(n,d) :
+t = int(input())
+while t > 0:
+    n,d = map(int,input().split())
     ans = 0
     for x in range(0,10**5):
         tmp = x + math.ceil(d/(x+1))
         if tmp <= n : 
             ans = 1
+
     print(["YES","NO"][1-ans])
+    t -= 1
 
-t = int(input())
-for _ in range(t):
-    solve(*map(int,input().split()) )
-    #print(*l)
-    #print(list(l))
-
-
-    
-    
