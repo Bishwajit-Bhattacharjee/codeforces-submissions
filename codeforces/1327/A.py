cc259465ff1ed0edb9cf7fd4d07f30c6c66f6881
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2020-03-25 22:13:00 +0600

    Add solution for problem `A - Sum of Odd Integers` Link: https://codeforces.com/contest/1327/problem/A Tags: math, *1100 Ref: https://codeforces.com/contest/1327/submission/74312446

--- codeforces/1327/A.py
+++ codeforces/1327/A.py
@@ -1,9 +1,5 @@
-import io
-import os
 import sys
-from io import BytesIO
-input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
-
+input = sys.stdin.readline
 t = int(input())
 for _ in range(t) :
     n,k = map(int,input().split())
