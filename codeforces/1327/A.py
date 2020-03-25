e5488fca9dbdb6f6bcd2a4b26658ebafd2eee9c7
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2020-03-25 22:10:00 +0600

    Add solution for problem `A - Sum of Odd Integers` Link: https://codeforces.com/contest/1327/problem/A Tags: math, *1100 Ref: https://codeforces.com/contest/1327/submission/74312157

--- codeforces/1327/A.py
+++ codeforces/1327/A.py
@@ -1,7 +1,6 @@
-import sys
-input = sys.stdin.readline
 t = int(input())
 for _ in range(t) :
     n,k = map(int,input().split())
     ans = n - (k-1)*(k-1)
     print(["YES","NO"][not(ans & 1 and ans > (2*k-3))])
+    
