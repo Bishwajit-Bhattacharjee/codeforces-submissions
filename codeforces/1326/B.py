2220949f5475dd40bff91f7c6102953047449adf
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2020-03-28 22:55:00 +0600

    Add solution for problem `B - Maximums` Link: https://codeforces.com/contest/1326/problem/B Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1326/submission/74659798

--- codeforces/1326/B.py
+++ codeforces/1326/B.py
@@ -1,10 +1,15 @@
 import sys
 input = sys.stdin.readline
-mx = 0
-input()
-for el in map(int,input().split()) :
-    print(el + mx); mx = max(mx,el+mx)
 
+n = int(input())
+x = [] 
+mx = 0
+v = list(map(int,input().split()))
+
+for el in v : 
+    x.append(el + mx)
+    mx = max(mx,el + mx)
+print(*x)
 
 
         
