1ef92109f18e774ab0be2239ec25568366e66b00
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2020-04-08 12:36:00 +0600

    Add solution for problem `A - Distinct Digits` Link: https://codeforces.com/contest/1228/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1228/submission/75807823

--- codeforces/1228/A.py
+++ codeforces/1228/A.py
@@ -1,8 +1,17 @@
 
+def diff(n):
+    dict = {}
+    while(n) : 
+        if (n % 10) in dict :
+            return False
+        dict[n%10] = 1
+        n //= 10
+    return True
+
 #print(diff(98766))
 l,r = map(int,input().split())
 for _ in range(l,r+1):
-    if len(set(str(_))) == len(str(_)) :
+    if diff(_) :
         print(_)
         exit(0)
 print("-1")
