7f048047bec1f243027ea77468eb5fa1240114f3
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2020-09-05 20:35:00 +0600

    Add solution for problem `D - Unmerge` Link: https://codeforces.com/contest/1382/problem/D Tags: dp, *1800 Ref: https://codeforces.com/contest/1382/submission/91948999

--- codeforces/1382/D.cpp
+++ codeforces/1382/D.cpp
@@ -1,33 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 int const N = 4010;
+int dp[N][N][3] ;
+vector<int> v,special ;
+int n;
+
+int DP(int pos, int tkn, int st){
+    if(tkn > n) return 0;
+    if(pos == 2*n) return tkn == n;
+    int &ret = dp[pos][tkn][st];
+    if(ret != -1) return ret;
+    ret = 0;
+    if(special[pos]){
+        ret |= DP(pos+1, tkn+(st == 1), st^1);
+        ret |= DP(pos+1, tkn+ (st == 0), st);
+    }
+    else {
+        ret |= DP(pos+1, tkn+(st == 0), st);
+    }
+    return ret;
+}
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     int t;
     cin >> t;
     while(t--){
-        int n;
         cin >> n;
-        vector<int> v(2*n);
-        for(int i = 0; i <  2*n; i++) cin >> v[i] ;
-        vector<int> imp;
-        imp.push_back(0);
-        int mx = v[0];
-        for(int i = 1; i < 2 * n;i++) {
-            if( v[i] > mx ) mx = v[i], imp.push_back(i);
-        }
-        map <int, int > cnt;
-        for(int i = 1; i < imp.size(); i++) cnt[imp[i]-imp[i-1]]++;
-        vector<int> need(2*n+5, 0), vis(2*n+5, 0);
-        vis[0] = true;
-        for(auto x : cnt){
-            int val = x.first , q = x.second;
-            fill(need.begin(),need.end(), 0);
-            for(int i = 0; i <= 2*n; i++){
-                if(!vis[i] and i >= val and vis[i - val] and need[i-val] < q)
-                    vis[i] = true, need[i] = need[i-val] + 1;
-            }
-        }
-        cout << (vis[n] ? "YES":"NO") << '\n';
+        v.resize(2*n), special = vector<int>(2*n,0);
+        for(int i = 0 ; i < 2 * n; i++) cin >> v[i] ;
+        special[0] = 1;
+        int id = 0;
+        for (int i = 1; i < 2 * n; i++)
+            if (v[i] > v[id])
+                special[i] = 1, id = i;
+
+        for(int i = 0; i <= 2*n; i++)
+            for(int j = 0; j <= 2*n; j++)
+                for(int k = 0; k < 3; k++)
+                    dp[i][j][k] = -1;
+ 
+        cout << (DP(1,1,0)?"YES":"NO") << '\n';
     }
 }
