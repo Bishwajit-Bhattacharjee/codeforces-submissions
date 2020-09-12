29ce80cb2e93ddad07ca37ef4aff1a7e5ef9ce4e
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2020-09-12 18:34:00 +0600

    Add solution for problem `G - Tree Modification` Link: https://codeforces.com/contest/1375/problem/G Tags: brute force, constructive algorithms, dfs and similar, graph matchings, graphs, trees, *2800 Ref: https://codeforces.com/contest/1375/submission/92577547

---
+++ codeforces/1375/G.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace std;
+using namespace __gnu_pbds;
+
+double INF = 1e100;
+double EPS = 1e-12;
+typedef long long int ll;
+
+typedef pair < int,int > PII;
+typedef pair < ll,ll > PLL;
+
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+#define F first
+#define S second
+
+
+ostream& operator<<(ostream & os, PLL h){
+	return os << "( " << h.F << ", " << h.S << " )" << endl;
+
+}
+PLL operator+ (PLL a, ll x)     {return {a.F + x, a.S + x} ;}
+PLL operator- (PLL a, ll x)     {return {a.F - x, a.S - x} ;}
+PLL operator* (PLL a, ll x)     {return {a.F * x, a.S * x} ;}
+PLL operator+(PLL x, PLL y) { return {x.F + y.F,x.S + y.S} ;}
+PLL operator-(PLL x,PLL y) { return {x.F - y.F, x.S - y.S} ;}
+PLL operator*(PLL x,PLL y) { return {x.F * y.F , x.S * y.S} ;}
+PLL operator%(PLL x,PLL y) { return {x.F % y.F, x.S % y.S} ;}
+
+
+ll const MOD = 1e9 + 7;
+
+ll bigmod(ll a, ll b){
+	if(!b) return 1;
+	ll x  = bigmod(a,b/2);
+
+	x = (x * x)%MOD;
+	if(b&1)
+		x = (x * a) %MOD;
+	return x;
+}
+
+vector < ll > primes;
+vector < bool > marks;
+
+void sieve(int n)
+{
+    marks.resize(n+10,0);
+	marks[1] = 1;
+	for(int i = 2; i < n; i++){
+		if(!marks[i]){
+			for(int j = 2*i; j < n; j += i){
+				marks[j] = 1;
+			}
+			primes.push_back(i);
+		}
+	}
+}
+int const N = 2e5 + 10;
+int col[2], lvl[N] ;
+vector<int> g[N] ;
+void dfs(int u, int l){
+	lvl[u] = l;
+	col[l&1]++;
+	for(auto v : g[u]){
+		if(lvl[v] == -1) dfs(v, l + 1);
+	}
+}
+int main(){
+	ios::sync_with_stdio(false); cin.tie(0);
+	int n;
+	cin >> n;
+	for(int i = 0; i < n - 1; i++){
+		int a,b;
+		cin >> a >> b;
+		g[a].push_back(b); g[b].push_back(a);
+	}
+	memset(lvl,-1,sizeof lvl);
+	dfs(1,0);
+	if(col[0] > col[1]) swap(col[0], col[1]);
+	cout << col[0] - 1 << '\n';
+	return 0;
+}
