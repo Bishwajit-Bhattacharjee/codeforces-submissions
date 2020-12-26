a0d112a9f3494d556b85111bc3d7adcb82cc5c7e
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2020-12-26 22:37:00 +0600

    Add solution for problem `A - Special Permutation` Link: https://codeforces.com/contest/1454/problem/A Tags: constructive algorithms, probabilities, *800 Ref: https://codeforces.com/contest/1454/submission/102413257

---
+++ codeforces/1454/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+
+void solve(){
+    int n;
+    cin >> n;
+    for (int i = 2; i <= n; i++) cout << i << " ";
+    cout << "1\n";
+}
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
