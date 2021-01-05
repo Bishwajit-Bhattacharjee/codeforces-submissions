9e425433ef7cf285194166c88ecf0834316ee18f
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2021-01-05 11:45:00 +0600

    Add solution for problem `G - Moving to the Capital` Link: https://codeforces.com/contest/1472/problem/G Tags: dfs and similar, dp, graphs, shortest paths, *2100 Ref: https://codeforces.com/contest/1472/submission/103344691

--- codeforces/1472/G.cpp
+++ codeforces/1472/G.cpp
@@ -3,74 +3,89 @@ using namespace std;
 using ll = long long int;
 
 int const N = 3e5 + 10;
-vector < int > g[N];
-int d[N];
-bool vis[N] ;
+vector < int > g[2][N];
+int dist[2][N];
+int vis[N];
+int dp[2][N];
 
-void bfs(int u) {
-    d[u] = 0;
+void bfs(int u, int pos){
+
     vis[u] = 1;
-    queue<int> q;
+    dist[pos][u] = 0;
+    queue < int > q;
     q.push(u);
-
     while (q.size()) {
         int v = q.front(); q.pop();
-
-        for (auto x : g[v]){
+        for (auto x : g[0][v]) {
             if (vis[x]) continue;
             vis[x] = 1;
-            d[x] = d[v] + 1;
+            dist[pos][x] = dist[pos][v] + 1;
             q.push(x);
         }
     }
+
+}
+
+int DP(int u, int st) {
+    int &ret = dp[st][u];
+    if (ret != -1) return ret;
+
+    ret = dist[0][u];
+
+    assert(ret != -1);
+
+    if (ret == -1) ret = INT_MAX;
+
+    for (auto x : g[0][u]) {
+
+        if (dist[0][u] < dist[0][x]) {
+            ret = min(ret, DP(x, st));
+        }
+        else {
+            if(st)
+                ret = min(ret, DP(x, 0));
+        }
+    }
+    return ret;
 }
 
 void solve(){
     int n, m;
     cin >> n >> m;
-    
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++){
         int u, v;
         cin >> u >> v;
-        g[u].push_back(v);
+        g[0][u].push_back(v);
+        g[1][v].push_back(u);
     }
+    for (int i = 1; i <= n; i++) {
+        vis[i] = 0; dist[0][i] = -1;
+    }    
+    bfs (1, 0);
 
-    fill(vis + 1, vis + n + 1, 0);    
-
-    bfs(1);
-
-    vector <int> ord(n);
-    for(int i = 0; i < n; i++) ord[i] = i + 1;
-
-    sort(ord.begin(), ord.end(), 
-        [](int l, int r)->bool {
-            return d[l] > d[r];
-        }
-    );
-    vector < int > dp(n+1, INT_MAX), dp1(n+1, INT_MAX);
-
-    for (auto x : ord) {
-        dp[x] = d[x];
-        for (auto y : g[x]){
-            if (d[y] > d[x]) dp[x] = min(dp[x], dp[y]);
-        }
-    }
-
-    for (auto x : ord) {
-        dp1[x] = dp[x];
-        for (auto y : g[x]){
-            if (d[y] <= d[x]) dp1[x] = min(dp1[x], dp[y]);
-            if (d[y] > d[x]) dp1[x] = min(dp1[x], dp1[y]);
-        }
-    }
+    for (int i = 0; i <= n; i++) dp[0][i] = dp[1][i] = -1;
 
     for (int i = 1; i <= n; i++) {
-        cout << dp1[i] << " " ;
+        cout << DP(i, 1) << " ";
     }
     cout << "\n";
+    // for (int i = 1; i <= n; i++) {
+    //     int ans = dist[1][i];
+    //     if (ans == -1) ans = INT_MAX;
+
+    //     for (auto x : g[0][i]) {
+    //         if (dist[0][i] < dist[0][x]) {
+    //             ans = min(ans, dist[1][x]);
+    //         }
+    //     }
+    //     cout << ans << " ";
+    //     // assert(ans != INT_MAX);
+    // }
+
+    // cout << "\n";
 
     for (int i = 0; i <= n; i++) {
-        g[i].clear();
+        g[0][i].clear(), g[1][i].clear();
     }
 }
 int main(){
