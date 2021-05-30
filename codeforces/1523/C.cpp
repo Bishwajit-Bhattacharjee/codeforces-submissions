7e6d7e7dd8a0a96d209131dcb88ca9cd23f85a0d
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2021-05-30 21:37:00 +0600

    Add solution for problem `C - Compression and Expansion` Link: https://codeforces.com/contest/1523/problem/C Tags: brute force, data structures, greedy, implementation, trees, *1600 Ref: https://codeforces.com/contest/1523/submission/117898942

---
+++ codeforces/1523/C.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace std;
+using namespace __gnu_pbds;
+
+double INF = 1e100;
+double EPS = 1e-12;
+typedef long long int ll;
+
+typedef pair < int,int > PII;
+typedef pair < ll,ll > PLL;
+
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// to generate a uniform random integer over a range (l,r), use 
+// int x = uniform_int_distribution<int>(l,r)(rng);
+
+#define F first
+#define S second
+#define all(v) (v).begin(),(v).end()
+
+ostream& operator<<(ostream & os, PLL h){
+	return os << "( " << h.F << ", " << h.S << " )" << endl;
+
+}
+PLL operator+ (PLL a, ll x)     {return {a.F + x, a.S + x} ;}
+PLL operator- (PLL a, ll x)     {return {a.F - x, a.S - x} ;}
+PLL operator* (PLL a, ll x)     {return {a.F * x, a.S * x} ;}
+PLL operator+(PLL x, PLL y) { return {x.F + y.F,x.S + y.S} ;}
+PLL operator-(PLL x,PLL y) { return {x.F - y.F, x.S - y.S} ;}
+PLL operator*(PLL x,PLL y) { return {x.F * y.F , x.S * y.S} ;}
+PLL operator%(PLL x,PLL y) { return {x.F % y.F, x.S % y.S} ;}
+
+
+ll const MOD = 1e9 + 7;
+
+ll bigmod(ll a, ll b){
+	if(!b) return 1;
+	ll x  = bigmod(a,b/2);
+
+	x = (x * x)%MOD;
+	if(b&1)
+		x = (x * a) %MOD;
+	return x;
+}
+
+vector < ll > primes;
+vector < bool > marks;
+
+void sieve(int n)
+{
+    marks.resize(n+10,0);
+	marks[1] = 1;
+	for(int i = 2; i < n; i++){
+		if(!marks[i]){
+			for(int j = 2*i; j < n; j += i){
+				marks[j] = 1;
+			}
+			primes.push_back(i);
+		}
+	}
+}
+
+void solve(){
+	int n;
+	cin >> n;
+	vector<int> val(n);
+	for (auto &x : val) cin >> x;
+
+	stack<vector<int>> v;
+	vector<vector<int>> ans;
+
+	v.push(vector<int>{1});
+	ans.push_back(vector<int>{1});
+
+	for (int i = 1; i < n; i++){
+		vector<int> cur;
+		if (val[i] == 1){
+			cur = v.top();
+			cur.push_back(1);
+		}
+		else {
+			while (v.top().back() + 1 != val[i]){
+				int len = v.top().size();
+				int last = v.top()[len-2];
+				while (v.top().size() == len && v.top()[len - 2] == last)
+					v.pop();
+			}
+			cur = v.top();
+			cur.back() = val[i];	
+		}
+		v.push(cur);
+		ans.push_back(cur);
+	}
+
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < ans[i].size(); j++){
+			cout << ans[i][j] << ".\n"[j+1 == ans[i].size()];
+		}
+	}
+
+}
+
+int main(){
+
+	ios::sync_with_stdio(false); cin.tie(0);
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+	
+	return 0;
+}
