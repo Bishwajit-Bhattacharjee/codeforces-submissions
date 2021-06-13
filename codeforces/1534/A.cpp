3dcbe1ff570e253c451937051371f9368c7af239
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2021-06-13 21:46:00 +0600

    Add solution for problem `A - Colour the Flag` Link: https://codeforces.com/contest/1534/problem/A Tags: brute force, implementation Ref: https://codeforces.com/contest/1534/submission/119342155

---
+++ codeforces/1534/A.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace std;
+using namespace __gnu_pbds;
+
+double INF = 1e100;
+double EPS = 1e-12;
+typedef long long int ll;
+
+typedef pair < int,int > PII;
+typedef pair < ll,ll > PLL;
+
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// to generate a uniform random integer over a range (l,r), use 
+// int x = uniform_int_distribution<int>(l,r)(rng);
+
+#define F first
+#define S second
+#define all(v) (v).begin(),(v).end()
+
+ostream& operator<<(ostream & os, PLL h){
+	return os << "( " << h.F << ", " << h.S << " )" << endl;
+
+}
+PLL operator+ (PLL a, ll x)     {return {a.F + x, a.S + x} ;}
+PLL operator- (PLL a, ll x)     {return {a.F - x, a.S - x} ;}
+PLL operator* (PLL a, ll x)     {return {a.F * x, a.S * x} ;}
+PLL operator+(PLL x, PLL y) { return {x.F + y.F,x.S + y.S} ;}
+PLL operator-(PLL x,PLL y) { return {x.F - y.F, x.S - y.S} ;}
+PLL operator*(PLL x,PLL y) { return {x.F * y.F , x.S * y.S} ;}
+PLL operator%(PLL x,PLL y) { return {x.F % y.F, x.S % y.S} ;}
+
+
+ll const MOD = 1e9 + 7;
+
+ll bigmod(ll a, ll b){
+	if(!b) return 1;
+	ll x  = bigmod(a,b/2);
+
+	x = (x * x)%MOD;
+	if(b&1)
+		x = (x * a) %MOD;
+	return x;
+}
+
+vector < ll > primes;
+vector < bool > marks;
+
+void sieve(int n)
+{
+    marks.resize(n+10,0);
+	marks[1] = 1;
+	for(int i = 2; i < n; i++){
+		if(!marks[i]){
+			for(int j = 2*i; j < n; j += i){
+				marks[j] = 1;
+			}
+			primes.push_back(i);
+		}
+	}
+}
+
+void solve(){
+	int n, m;
+	cin >> n >> m;
+	vector<string> g(n), c1(n, string(m, '.')), c2(n, string(m, '.'));
+	for (int i = 0; i < n; i++) cin >> g[i];
+
+	bool done = 0;	
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < m; j++){
+			if ((i+j) & 1){
+				c1[i][j] = 'R';
+				c2[i][j] = 'W';
+			}
+			else {
+				c1[i][j] = 'W';
+				c2[i][j] = 'R';
+			}
+		}
+	}
+	int ans = -1;
+	bool d = 1;
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < m; j++){
+			if (g[i][j] != '.' && g[i][j] != c1[i][j]){
+				d = 0;	
+			}
+		}
+	}
+	if (d) ans = 0;
+	d = 1;
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < m; j++){
+			if (g[i][j] != '.' && g[i][j] != c2[i][j]){
+				d = 0;	
+			}
+		}
+	}
+	if (d) ans = 1;
+
+	if (ans == -1) {
+		cout << "NO\n";
+	}
+	else {
+		cout << "YES\n";
+		if (ans == 1){
+
+			for (int i = 0; i < n; i++){
+				for (int j = 0; j < m; j++){
+					cout << c2[i][j];
+				}
+				cout << "\n";
+			}
+		} else {
+			for (int i = 0; i < n; i++){
+				for (int j = 0; j < m; j++){
+					cout << c1[i][j];
+				}
+				cout << "\n";
+			}
+		}
+	}
+
+}
+
+int main(){
+
+	ios::sync_with_stdio(false); cin.tie(0);
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+	
+	return 0;
+}
